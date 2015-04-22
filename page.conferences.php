7df94d42293ca4b435cca3c09c3e67c763631438
Andrew Nagy <andrew.nagy@the159.com>
2015-04-21 20:28:21 -0400

    FREEPBX-8993 Popovers in FreePBX 13

--- page.conferences.php
+++ page.conferences.php
@@ -27,14 +27,14 @@ switch($request['view']){
 	<h1><?php echo $heading ?></h1>
 	<div class = "display full-border">
 		<div class="row">
-			<div class="col-sm-9">
+			<div class="<?php echo $request['fw_popover']?'col-sm-12':'col-sm-9'?>">
 				<div class="fpbx-container">
 					<div class="display full-border">
 						<?php echo $content ?>
 					</div>
 				</div>
 			</div>
-			<div class="col-sm-3 hidden-xs bootnav">
+			<div class="col-sm-3 hidden-xs bootnav <?php echo $request['fw_popover']?'hidden':''?>">
 				<div class="list-group">
 					<?php echo load_view(__DIR__.'/views/bootnav.php', array('request' => $request));?>
 				</div>
